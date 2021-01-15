3ce25e2e2b6e076558122f9c762cd1a72207a144
aynur <galimovaynur@protonmail.com>
2021-01-15 22:37:26 +0500

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#420008]

--- src/main/java/ru/job4j/html/SqlRuParser.java
+++ src/main/java/ru/job4j/html/SqlRuParser.java
@@ -9,17 +9,21 @@ import ru.job4j.utils.SqlRuDateConvertor;
 import java.io.IOException;
 
 public class SqlRuParser {
-    public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements posts = doc.select(".postslisttopic");
-        Elements updated = doc.select(".altCol:nth-child(even)");
+    private static final String BASE_URL = "https://www.sql.ru/forum/job-offers/";
 
-        for (int i = 0; i < posts.size(); i++) {
-            Element href = posts.get(i).child(0);
-            Element date = updated.get(i);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(SqlRuDateConvertor.convertor(date.text()));
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 5; i++) {
+            Document doc = Jsoup.connect(BASE_URL + (i + 1)).get();
+            Elements posts = doc.select(".postslisttopic");
+            Elements updated = doc.select(".altCol:nth-child(even)");
+            for (int j = 0; j < posts.size(); j++) {
+                Element href = posts.get(j).child(0);
+                Element date = updated.get(j);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(SqlRuDateConvertor.convertor(date.text()));
+            }
         }
+
     }
 }
