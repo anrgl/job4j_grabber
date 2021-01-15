bd65d270e8ea031bfcfdd8b3e3f4def7c242142e
aynur <galimovaynur@protonmail.com>
2021-01-15 22:49:00 +0500

    2.2. Модель данных - Post. [#420009]

---
+++ src/main/java/ru/job4j/models/Post.java
@@ -0,0 +1,34 @@
+package ru.job4j.models;
+
+import java.util.Date;
+
+public class Post {
+    private int id;
+    private String url;
+    private String title;
+    private Date date;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
